ff102ac09194f922b5fe10feb10a4201cb821c9b
Irakli Barbakadze <ibarbakadze@credo.ge>
2026-05-24 20:07:08 +0400

    [Irakli B.] Add Homework3.java with author info

---
+++ TestAutomationCourse/src/main/java/Homework3.java
@@ -0,0 +1,13 @@
+/**
+ * Homework 3 - Github PR Workflow demonstaration
+ * @author Irakli Barbakadze
+ */
+
+public class Homework3 {
+    public static void main(String[] args) {
+        String author = "Irakli Barbakadze";
+        int currentYear = 2026;
+        System.out.println("[" + author + "] First Pull Request - " + currentYear);
+    }
+
+}
